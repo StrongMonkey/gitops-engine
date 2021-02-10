91c9646d49da4007076fb9b83e9e5816d7ebbeb7
Daishan <daishan@rancher.com>
2021-02-10 12:45:00 -0700

    Don't use kubernetes package

--- pkg/diff/diff.go
+++ pkg/diff/diff.go
@@ -6,11 +6,17 @@ package diff
 
 import (
 	"bytes"
+	"crypto/md5"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"hash"
 	"reflect"
+	"sort"
 
+	"github.com/davecgh/go-spew/spew"
+	jsonutil "github.com/argoproj/gitops-engine/pkg/utils/json"
+	kubescheme "github.com/argoproj/gitops-engine/pkg/utils/kube/scheme"
 	jsonpatch "github.com/evanphx/json-patch"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -18,10 +24,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/jsonmergepatch"
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/kubernetes/pkg/api/v1/endpoints"
-
-	jsonutil "github.com/argoproj/gitops-engine/pkg/utils/json"
-	kubescheme "github.com/argoproj/gitops-engine/pkg/utils/kube/scheme"
 )
 
 const couldNotMarshalErrMsg = "Could not unmarshal to object of type %s: %v"
@@ -517,7 +519,7 @@ func normalizeEndpoint(un *unstructured.Unstructured, o options) {
 		return
 	}
 
-	endpoints.SortSubsets(ep.Subsets)
+	sortSubsets(ep.Subsets)
 
 	newObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&ep)
 	if err != nil {
@@ -701,4 +703,81 @@ func remarshal(obj *unstructured.Unstructured, o options) *unstructured.Unstruct
 	// Remove all default values specified by custom formatter (e.g. creationTimestamp)
 	unstrBody = jsonutil.RemoveMapFields(obj.Object, unstrBody)
 	return &unstructured.Unstructured{Object: unstrBody}
+}
+
+// SortSubsets sorts an array of EndpointSubset objects in place.  For ease of
+// use it returns the input slice.
+func sortSubsets(subsets []corev1.EndpointSubset) []corev1.EndpointSubset {
+	for i := range subsets {
+		ss := &subsets[i]
+		sort.Sort(addrsByIPAndUID(ss.Addresses))
+		sort.Sort(addrsByIPAndUID(ss.NotReadyAddresses))
+		sort.Sort(portsByHash(ss.Ports))
+	}
+	sort.Sort(subsetsByHash(subsets))
+	return subsets
+}
+
+func hashObject(hasher hash.Hash, obj interface{}) []byte {
+	deepHashObject(hasher, obj)
+	return hasher.Sum(nil)
+}
+
+type subsetsByHash []corev1.EndpointSubset
+
+func (sl subsetsByHash) Len() int      { return len(sl) }
+func (sl subsetsByHash) Swap(i, j int) { sl[i], sl[j] = sl[j], sl[i] }
+func (sl subsetsByHash) Less(i, j int) bool {
+	hasher := md5.New()
+	h1 := hashObject(hasher, sl[i])
+	h2 := hashObject(hasher, sl[j])
+	return bytes.Compare(h1, h2) < 0
+}
+
+type addrsByIPAndUID []corev1.EndpointAddress
+
+func (sl addrsByIPAndUID) Len() int      { return len(sl) }
+func (sl addrsByIPAndUID) Swap(i, j int) { sl[i], sl[j] = sl[j], sl[i] }
+func (sl addrsByIPAndUID) Less(i, j int) bool {
+	return LessEndpointAddress(&sl[i], &sl[j])
+}
+
+type portsByHash []corev1.EndpointPort
+
+func (sl portsByHash) Len() int      { return len(sl) }
+func (sl portsByHash) Swap(i, j int) { sl[i], sl[j] = sl[j], sl[i] }
+func (sl portsByHash) Less(i, j int) bool {
+	hasher := md5.New()
+	h1 := hashObject(hasher, sl[i])
+	h2 := hashObject(hasher, sl[j])
+	return bytes.Compare(h1, h2) < 0
+}
+
+// LessEndpointAddress compares IP addresses lexicographically and returns true if first argument is lesser than second
+func LessEndpointAddress(a, b *corev1.EndpointAddress) bool {
+	ipComparison := bytes.Compare([]byte(a.IP), []byte(b.IP))
+	if ipComparison != 0 {
+		return ipComparison < 0
+	}
+	if b.TargetRef == nil {
+		return false
+	}
+	if a.TargetRef == nil {
+		return true
+	}
+	return a.TargetRef.UID < b.TargetRef.UID
+}
+
+// DeepHashObject writes specified object to hash using the spew library
+// which follows pointers and prints actual values of the nested objects
+// ensuring the hash does not change when a pointer changes.
+func deepHashObject(hasher hash.Hash, objectToWrite interface{}) {
+	hasher.Reset()
+	printer := spew.ConfigState{
+		Indent:         " ",
+		SortKeys:       true,
+		DisableMethods: true,
+		SpewKeys:       true,
+	}
+	printer.Fprintf(hasher, "%#v", objectToWrite)
 }
